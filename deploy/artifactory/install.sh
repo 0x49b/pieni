393d30df328d9297ad1be7623e676186b6317457
Florian Thiévent <florian.thievent@sbb.ch>
2019-01-25 09:02:59 +0100

    added artifactory to openshift

---
+++ deploy/artifactory/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ "_${PIENI_PROJECT}" = "_" ]; then
+    echo "Need to set version with export PIENI_PROJECT"
+    exit 1
+fi
+
+echo "Installing secret"
+
+# switch to kib project
+oc project ${PIENI_PROJECT}
+
+# Auth in config.json with folowing pattern as base64 encoded string:
+# <user>:<password>
+
+# Secret erstellen
+oc secrets new external-registry .dockerconfigjson=config.json
+# Secrets dem Default Service-Account zuweisen
+oc secrets add serviceaccount/default secrets/external-registry --for=pull
+
+unset IENI_PROJECT
---
+++ deploy/artifactory/remove.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ "_${PIENI_PROJECT}" = "_" ]; then
+    echo "Need to set version with export PIENI_PROJECT"
+    exit 1
+fi
+
+echo "remove secret"
+
+# switch to kib project
+oc project ${PIENI_PROJECT}
+
+# delete pull secret
+oc delete secret external-registry
+
+unset PIENI_PROJECT
